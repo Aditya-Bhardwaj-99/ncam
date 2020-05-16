133c8149c119fde0ae45e0e4122639ac09839e0e
Aditya-Bhardwaj-99 <bhardwaj.aditya99@gmail.com>
2020-05-16 21:48:11 +0530

    first

---
+++ index.js
@@ -0,0 +1,21 @@
+const http=require("http");
+const express = require('express');
+const app = express();
+const server=http.createServer(app);
+const websokcet = require("ws");
+const wss = new websokcet.Server({server:server,path:'/picam'});
+
+wss.broadcast = function broadcast(msg) {
+    console.log(msg);
+    wss.clients.forEach(function each(client) {
+        client.send(msg);
+     });
+ };
+
+wss.on('connection',function(ws){
+    ws.on('message',function(data){
+        wss.broadcast(data);
+    })
+})
+
+server.listen(process.env.PORT || 8001);
